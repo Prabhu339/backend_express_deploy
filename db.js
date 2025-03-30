029a5fa58748202e4248e46e869741b2c80e0776
prabhu339 <magapuchinni@gmail.com>
2025-03-30 14:57:18 +0530

    backend

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose")
-const uri="mongodb://localhost:27017/ekart"
+//const uri="mongodb://localhost:27017/ekart"
+const uri="mongodb+srv://magapuchinni:IC1bvb8B8AiN6LBi@cluster0.fusgiuv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 //const dbUrl="mongodb+srv://magapuchinni:sr2j9PpK0hO53VP5@cluster0.5azkm.mongodb.net/express"
 mongoose.connect(uri)
 .then(()=>console.log("database connected successfully"))
